5ec2abbb6a8a08939bd75221947e096b0c60b462
Doug Brown <doug.brown@academy.com>
2018-02-08 16:42:39 -0600

    Add more styling to Locations as well as early styling for overall display

--- src/js/locations/Location.js
+++ src/js/locations/Location.js
@@ -1,18 +1,24 @@
 // Location class for Electronic Detective
 
 import React from 'react';
+import "../../css/locations/Location.css";
 
 
 const Location = function (props) {
+    const attendees = props.locationData.attendees;
+    const address = props.locationData.address;
+    const weapon = props.locationData.weapon;
+    const scene = props.locationData.scene;
+
     return (
-        <div className="Location">
+        <div className={scene ? "Scene" : "Location"}>
             <header className="Location-header">
                 <h2 className="Location-id">{props.locationData.id}</h2>
             </header>
             <p className="Location-name">
                 <strong>{props.locationData.name}</strong>
             </p>
-            <table>
+            <table className="AttendeesTable">
                 <thead>
                 <tr>
                     <th>Men</th>
@@ -21,25 +27,20 @@ const Location = function (props) {
                 </thead>
                 <tbody>
                 <tr>
-                    <td>{props.locationData.attendees.men[0] && props.locationData.attendees.men[0].name}</td>
-                    <td>{props.locationData.attendees.women[0] && props.locationData.attendees.women[0].name}</td>
+                    <td>{((attendees.men[0] && attendees.men[0].name) || ` `)}</td>
+                    <td>{((attendees.women[0] && attendees.women[0].name) || ` `)}</td>
                 </tr>
                 <tr>
-                    <td>{props.locationData.attendees.men[1] && props.locationData.attendees.men[1].name}</td>
-                    <td>{props.locationData.attendees.women[1] && props.locationData.attendees.women[1].name}</td>
+                    <td>{((attendees.men[1] && attendees.men[1].name) || ` `)}</td>
+                    <td>{((attendees.women[1] && attendees.women[1].name) || ` `)}</td>
                 </tr>
                 </tbody>
             </table>
             <div className="Location-address">
-                <div className="Location-side">
-                    {props.locationData.address.side}{` Side`}
-                </div>
-                <div className="Location-town">
-                    {props.locationData.address.town}
-                </div>
+                {address.side}{` Side / `}{address.town}
             </div>
             <div className="Location-weapon">
-                Weapon: {props.locationData.weapon.type}
+                {weapon.type && `Weapon: `}{weapon.type}
             </div>
         </div>
     );
