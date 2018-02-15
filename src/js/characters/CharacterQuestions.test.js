c50c461df899cd182389045cfc91b91773f50aa3
Doug Brown <doug.brown@academy.com>
2018-02-15 16:21:05 -0600

    Add unit testing for CharacterQuestions component

--- src/js/characters/CharacterQuestions.test.js
+++ src/js/characters/CharacterQuestions.test.js
@@ -22,7 +22,7 @@ describe("CharacterQuestions", () => {
     }
 
     beforeEach(() => {
-        const questionArr = [
+        const questionsArr = [
             {
                 "id": 1,
                 "subject": "murderer",
@@ -78,9 +78,30 @@ describe("CharacterQuestions", () => {
         mountedCharacterQuestions = undefined;
     })
 
-    it("contains five questions", () => {
-        const divs = characterQuestions().find(".Question");
+    it("contains five questions as unclicked links", () => {
+        props.interrogate = true;
+        const divs = characterQuestions().find(".Question-unclicked");
         divs.length.should.equal(5);
     });
+
+    it("shows two questions as clicked links and three as unclicked links", () => {
+        props.clickedState = [false, true, false, true, false];
+        props.interrogate = true;
+        let divs = characterQuestions().find(".Question a.Question-unclicked");
+        divs.length.should.equal(3);
+        divs = characterQuestions().find(".Question a.Question-clicked");
+        divs.length.should.equal(2);
+    });
+
+    it("shows two questions as clicked text and three as unclicked text", () => {
+        props.clickedState = [false, true, false, true, false];
+        props.allowed = 2;
+        props.asked = 2;
+        props.interrogate = true;
+        let divs = characterQuestions().find(".Question span.Question-unclicked");
+        divs.length.should.equal(3);
+        divs = characterQuestions().find(".Question span.Question-clicked");
+        divs.length.should.equal(2);
+    })
 
 });
