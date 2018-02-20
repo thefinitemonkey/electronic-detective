736cec5e90e58eea3f5e6e6a305a2e7a0cca7e50
Doug Brown <doug.brown@academy.com>
2018-02-20 08:46:14 -0600

    Fix misspelling in CharacterAlibi.test.js file name and add static class for answer logic

--- src/js/characters/Character.js
+++ src/js/characters/Character.js
@@ -4,6 +4,7 @@
 import React, { Component } from 'react';
 import "../../css/characters/Character.css";
 import CharacterQuestions from "./CharacterQuestions";
+import CharacterAnswerLogic from "./CharacterAnswerLogic";
 import CharacterAlibi from "./CharacterAlibi";
 import ErrorBoundary from "../ErrorBoundary";
 
@@ -40,34 +41,34 @@ class Character extends Component {
         // Need to check which function to run for the selected question and then post back the answer
         switch (question.answerFunction) {
             case "checkEastSide":
-                this.checkEastSide(question.subject, question.responseText);
+                this.setState(CharacterAnswerLogic.checkEastSide(this.state, question.subject, question.responseText));
                 break;
             case "checkMurdererGender":
-                this.checkMurdererGender(question.subject, question.responseText);
+                this.setState(CharacterAnswerLogic.checkMurdererGender(this.state, question.subject, question.responseText));
                 break;
             case "checkTownLocation":
-                this.checkTownLocation(question.subject, question.responseText);
+                this.setState(CharacterAnswerLogic.checkTownLocation(this.state, question.subject, question.responseText));
                 break;
             case "checkMurderWeapon":
-                this.checkMurderWeapon(question.subject, question.responseText);
+                this.setState(CharacterAnswerLogic.checkMurderWeapon(this.state, question.subject, question.responseText));
                 break;
             case "check38Location":
-                this.check38Location(question.subject, question.responseText);
+                this.setState(CharacterAnswerLogic.check38Location(this.state, question.subject, question.responseText));
                 break;
             case "check45Location":
-                this.check45Location(question.subject, question.responseText);
+                this.setState(CharacterAnswerLogic.check45Location(this.state, question.subject, question.responseText));
                 break;
             case "checkEmptySeat":
-                this.checkEmptySeat(question.subject, question.responseText);
+                this.setState(CharacterAnswerLogic.checkEmptySeat(this.state, question.subject, question.responseText));
                 break;
             case "checkPlaceNames":
-                this.checkPlaceNames(question.subject, question.responseText);
+                this.setState(CharacterAnswerLogic.checkPlaceNames(this.state, question.subject, question.responseText));
                 break;
             case "checkWeaponLocation":
-                this.checkWeaponLocation(question.subject, question.responseText);
+                this.setState(CharacterAnswerLogic.checkWeaponLocation(this.state, question.subject, question.responseText));
                 break;
             case "checkPrints":
-                this.checkPrints(question.subject, question.responseText);
+                this.setState(CharacterAnswerLogic.checkPrints(this.state, question.subject, question.responseText));
                 break;
             default:
         }
@@ -75,133 +76,7 @@ class Character extends Component {
     };
 
 
-    checkEastSide = (subject, response) => {
-        // Check whether the suspect or murderer was on the East side of town and display the
-        // appropriate response for the question
-        const side = subject === "suspect" ?
-            this.state.characterData.location.address.side :
-            this.state.murdererData.location.address.side;
 
-        const display = side === "East" ? response.affirmative : response.negative;
-
-        this.setState({ answer: display });
-    };
-
-
-    checkMurdererGender = (subject, response) => {
-        // Check whether the murderer is male and display the appropriate response
-        const display = this.state.murdererData.gender === "M" ? response.affirmative : response.negative;
-
-        this.setState({ answer: display });
-    };
-
-
-    checkTownLocation = (subject, response) => {
-        // Get the part of town the subject went to and append it to the affirmative
-        // response for display (there is no negative response)
-        let display;
-        subject === "murderer" ?
-            display = response.affirmative + this.state.murdererData.location.address.town :
-            display = response.affirmative + this.state.characterData.location.address.town;
-
-        this.setState({ answer: display });
-    };
-
-
-    checkMurderWeapon = (subject, response) => {
-        // Check the victim to see whether the weapon used was a .38 and display the response
-        const display = this.state.characterData.weapon.type === ".38" ?
-            response.affirmative : response.negative;
-
-        this.setState({ answer: display });
-    };
-
-
-    check38Location = (subject, response) => {
-        // Display the location of the .38 from the collection of weapons data
-        let weapon;
-        for (weapon of this.state.weaponData) {
-            if (weapon.type === ".38") break;
-        }
-
-        const display = response.affirmative + weapon.location.name;
-        this.setState({ answer: display });
-    };
-
-
-    check45Location = (subject, response) => {
-        // Display the location of the .45 from the collection of weapons data
-        let weapon;
-        for (weapon of this.state.weaponData) {
-            if (weapon.type === ".45") break;
-        }
-
-        const display = response.affirmative + weapon.location.name;
-        this.setState({ answer: display });
-    };
-
-
-    checkEmptySeat = (subject, response) => {
-        // Display the location name of the place with only three suspects
-        let location;
-        for (location of this.state.locationData) {
-            if (location.attendees.men.length + location.attendees.women.length === 3) break;
-        }
-
-        const display = response.affirmative + location.name;
-        this.setState({ answer: display });
-    };
-
-
-    checkPlaceNames = (subject, response) => {
-        // Check if the murderer was at location A, B, or C and respond appropriately
-        const locID = subject === "murderer" ?
-            this.state.murdererData.location.id :
-            this.state.characterData.location.id;
-
-        const display = (locID === "A" || locID === "B" || locID === "C") ?
-            response.affirmative : response.negative;
-        this.setState({ answer: display });
-    };
-
-
-    checkWeaponLocation = (subject, response) => {
-        // Check whether the character was at a location with a weapon
-        const display = this.state.characterData.location.weapon.type === null ?
-            response.negative : response.positive;
-
-        this.setState({ answer: display });
-    };
-
-
-    checkPrints = (subject, response) => {
-        // This one is trickier. Only people who were at a location with a weapon can answer.
-        if (this.state.characterData.location.weapon.type === null ||
-            this.state.characterData.location.weapon.type !== subject) {
-            this.setState({ answer: response.unknown });
-            return;
-        }
-
-        // Get the weapon for the character's location
-        const weapon = this.state.characterData.location.weapon;
-
-        // If the character is the same gender as the murderer then they have to give a truthful answer
-        if (this.state.characterData.gender === this.state.murdererData.gender) {
-            const display = weapon.print % 2 === 1 ? response.affirmative : response.negative;
-            this.setState({ answer: display });
-            return;
-        }
-
-        // If the character is not the same gender as the murderer then they might lie. It's a coin toss.
-        const lie = this.getRandomInt(2);
-        const display = lie ? response.negative : response.affirmative;
-        this.setState({ answer: display });
-    };
-
-
-    getRandomInt = (max) => {
-        return Math.floor(Math.random() * Math.floor(max));
-    }
 
 
     handleSuspectClick = (e, data) => {
