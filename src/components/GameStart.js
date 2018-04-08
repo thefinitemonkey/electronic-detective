4eb4b1a976efc24828476736c996012209face2a
Doug Brown <dougb@finitemonkey.com>
2018-04-08 16:56:50 -0500

    feat: Add picture frame and styling to GameStart

--- src/components/GameStart.js
+++ src/components/GameStart.js
@@ -29,27 +29,36 @@ class GameStart extends PureComponent {
       <div className={container}>
         <div className={spacertop} />
         <div className={[spacermid].join(" ")}>
-          <h2 className={[h2].join(" ")}>The Crime</h2>
+          <h2 className={[h2, h2crime].join(" ")}>The Crime</h2>
           <div className={centeredcontainer}>
             <div>
-            <img src={imgSource} alt={`Victim`} />
+              <img src={imgSource} alt={`Victim`} />
+            </div>
+            <div className={framecontainer}>
+              <img
+                src={`/images/funeral_frame.png`}
+                alt={`Frame`}
+                width={`300px`}
+              />
             </div>
           </div>
-          <div>
-            <span className={bodyStrong}>
+          <div className={crimeTextArea}>
+            <span className={[bodyStrong, crimeText].join(" ")}>
               {victim.name} (#{victimId})
             </span>{" "}
-            <span className={body}>was murdered at the</span>{" "}
-            <span className={bodyStrong}>{scene}</span>!
+            <span className={[body, crimeText].join(" ")}>
+              was murdered at the
+            </span>{" "}
+            <span className={[bodyStrong, crimeText].join(" ")}>{scene}</span>!
           </div>
-          <div className={rightcontainer}>
-          <div className={startGameButton}>
-            <RaisedButton
-              label={`Start Investigation`}
-              primary={true}
-              onClick={() => this.setPlayerTurn(0)}
-            />
-          </div>
+          <div className={centeredcontainer}>
+            <div className={startGameButton}>
+              <RaisedButton
+                label={`Start Investigation`}
+                primary={true}
+                onClick={() => this.setPlayerTurn(0)}
+              />
+            </div>
           </div>
         </div>
         <div className={spacerbottom} />
@@ -57,6 +66,10 @@ class GameStart extends PureComponent {
     );
   };
 }
+
+const h2crime = css`
+  margin-bottom: 55px;
+`;
 
 const container = css`
   width: 100%;
@@ -69,15 +82,24 @@ const container = css`
   min-height: 600px;
 `;
 
+/*
 const rightcontainer = css`
   display: flex;
   justify-content: flex-end;
-`
+`;
+*/
 
 const centeredcontainer = css`
   display: flex;
   justify-content: center;
-`
+  position: relative;
+`;
+
+const framecontainer = css`
+  position: absolute;
+  left: -62px;
+  top: -40px;
+`;
 
 const startGameButton = css`
   margin-top: 25px;
@@ -93,6 +115,16 @@ const spacermid = css`
 
 const spacerbottom = css`
   flex: 2;
+`;
+
+const crimeTextArea = css`
+  max-width: 205px;
+  margin-top: 75px;
+`;
+
+const crimeText = css`
+  margin-top: 15px;
+  font-size: 16px;
 `;
 
 function mapStateToProps(game) {
