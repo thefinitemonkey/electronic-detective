bec94bc8ab24a0a56a6dc5de9845b407e5b53be7
Doug Brown <doug.brown@academy.com>
2018-04-09 11:29:15 -0500

    feat: Add RulesDialog for rules display

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,33 @@ import StartTurn from "./StartTurn";
 import Unsolved from "./Unsolved";
 import Solved from "./Solved";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import { h1, app } from "../utils/globalcss";
+import { h1, app, body } from "../utils/globalcss";
+import { css } from "react-emotion";
+import RulesDialog from "./RulesDialog";
 
 class App extends Component {
-  shouldComponentUpdate = props => {
-    const shouldRender = props.game.screen !== this.props.game.screen;
+  state = { dialogOpen: false };
+
+  shouldComponentUpdate = (props, state) => {
+    const shouldRender =
+      props.game.screen !== this.props.game.screen ||
+      state.dialogOpen !== this.state.dialogOpen;
     this.props = props;
     return shouldRender;
+  };
+
+  handleShowRules = e => {
+    e.preventDefault();
+    this.handleDialogOpen();
+  };
+
+  handleDialogOpen = () => {
+    console.log("Handling dialog open");
+    this.setState({ dialogOpen: true });
+  };
+
+  handleDialogClose = () => {
+    this.setState({ dialogOpen: false });
   };
 
   render = () => {
@@ -49,12 +69,45 @@ class App extends Component {
 
     return (
       <div className={app}>
-        <h1 className={h1}>Electronic Detective</h1>
-        <MuiThemeProvider>{renderItem}</MuiThemeProvider>
+        <div className={appHeader}>
+          <div>
+            <h1 className={h1}>Electronic Detective</h1>
+          </div>
+          <div>
+            <a
+              href="showRules"
+              className={[body, rulesLink].join(" ")}
+              onClick={e => this.handleShowRules(e)}
+            >
+              Show rules
+            </a>
+          </div>
+        </div>
+        <MuiThemeProvider>
+          {renderItem}
+          <RulesDialog
+            dialogOpen={this.state.dialogOpen}
+            handleDialogClose={this.handleDialogClose}
+            handleDialogOpen={this.handleDialogOpen}
+          />>
+        </MuiThemeProvider>
       </div>
     );
   };
 }
+
+const appHeader = css`
+  display: flex;
+  flex: 1;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: baseline;
+`;
+
+const rulesLink = css`
+  color: blue;
+  text-decoration: none;
+`;
 
 function mapStateToProps(game) {
   return { game };
---
+++ src/components/RulesDialog.js
@@ -0,0 +1,77 @@
+import React, { PureComponent } from "react";
+import Dialog from "material-ui/Dialog";
+import RaisedButton from "material-ui/RaisedButton";
+import { body, bodyStrong, bodyCondensed } from "../utils/globalcss";
+
+class RulesDialog extends PureComponent {
+  state = {
+    dialogOpen: this.props.dialogOpen
+  };
+
+  componentWillReceiveProps = props => {
+      console.log("Received props", props);
+    this.setState({ dialogOpen: props.dialogOpen || false });
+  };
+
+  handleDialogOpen = () => {
+    this.props.handleDialogOpen();
+  };
+
+  handleDialogClose = () => {
+    this.props.handleDialogClose();
+  };
+
+  render = () => {
+    const actions = [
+      <RaisedButton label="Close" primary={true} onClick={this.handleDialogClose} />
+    ];
+
+    return (
+      <div>
+        <Dialog
+          title="Electronic Detective Rules"
+          actions={actions}
+          modal={false}
+          open={this.state.dialogOpen}
+          onRequestClose={this.handleDialogClose}
+          autoScrollBodyContent={true}
+        >
+          <div>
+            <div className={bodyCondensed}>Things to know</div>
+            <div className={body}>
+              <ul>
+                <li>Only one murderer</li>
+                <li>Only one victim</li>
+                <li>One two four players</li>
+                <li>
+                  The murderer will <span className={bodyStrong}>never</span> be where a weapon has been dumped
+                </li>
+                <li>
+                  No clues will ever be found at the crime scene (nobody said
+                  these rules made complete sense)
+                </li>
+                <li>
+                  Everybody answers questions honestly, even the murderer, with
+                  one exception
+                </li>
+                <li>
+                  Only a suspect of the <span className={bodyStrong}>same gender as the killer</span> is guaranteed
+                  to tell the truth about the fingerprints on the murder weapon
+                  (really, nobody said these rules made complete sense)
+                </li>
+                <li>If you guess wrong about the murderer, you're out</li>
+                <li>First person to correctly identify the murderer wins</li>
+                <li>
+                  Everyone gets to know what a suspect says in their initial
+                  statement. Questioning a suspect is private.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Dialog>
+      </div>
+    );
+  };
+}
+
+export default RulesDialog;
