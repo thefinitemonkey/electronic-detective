4f1fa0ec32a6d5da9eac2fc82c9faafd3a12ca28
Doug Brown <doug.brown@academy.com>
2018-04-13 08:28:47 -0500

    feat: Abstract network calls from Saga code

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,36 +1,20 @@
 import { call, all, put, take } from "redux-saga/effects";
 import { GET_SETUP_DATA, RECEIVE_GET_SETUP_DATA } from "../actions/index";
+import {getSetupDataFile} from "../api/Api";
 //import * as Api from "../api/Api";
 
 export function* getSetupDataSaga() {
   yield take(GET_SETUP_DATA);
 
-  const [sh, ch, lo, qu, we, ad] = yield all([
-    call(fetch, "/json/casesheet.json"),
-    call(fetch, "/json/characters.json"),
-    call(fetch, "/json/locations.json"),
-    call(fetch, "/json/questions.json"),
-    call(fetch, "/json/weapons.json"),
-    call(fetch, "/json/addresses.json")
+  const [sheet, characters, locations, questions, weapons, addresses] = yield all([
+    call(getSetupDataFile, "/json/casesheet.json"),
+    call(getSetupDataFile, "/json/characters.json"),
+    call(getSetupDataFile, "/json/locations.json"),
+    call(getSetupDataFile, "/json/questions.json"),
+    call(getSetupDataFile, "/json/weapons.json"),
+    call(getSetupDataFile, "/json/addresses.json")
   ]);
 
-  const [
-    sheet,
-    characters,
-    locations,
-    questions,
-    weapons,
-    addresses
-  ] = yield all([
-    (sh.json()),
-    (ch.json()),
-    (lo.json()),
-    (qu.json()),
-    (we.json()),
-    (ad.json())
-  ]);
-
-  //const setupData = yield call(Api.getSetupData);
   yield put({
     type: RECEIVE_GET_SETUP_DATA,
     setupData: { sheet, characters, locations, questions, weapons, addresses }
