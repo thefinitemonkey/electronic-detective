4f01a4dff657c1dde30a4aa894a8302b8e31fe7d
Doug Brown <doug.brown@academy.com>
2018-04-23 08:32:53 -0500

    feat: Add unit testing for Facts

--- src/components/casesheet/Facts.test.js
+++ src/components/casesheet/Facts.test.js
@@ -65,9 +65,40 @@ describe("Facts", () => {
       });
     });
 
-    it("calls updatePlayerClues with correct data when selection changes", () => {
+    it("renders a RadioGroup for murdererSex", () => {
+      expect(mountedFacts.find("RadioGroup[name='murdererSex']").length).toBe(
+        1
+      );
+    });
+
+    it("renders a RadioGroup for murdererSide", () => {
+      expect(mountedFacts.find("RadioGroup[name='murdererSide']").length).toBe(
+        1
+      );
+    });
+
+    it("renders a RadioGroup for murdererTown", () => {
+      expect(mountedFacts.find("RadioGroup[name='murdererTown']").length).toBe(
+        1
+      );
+    });
+
+    it("renders a RadioGroup for murdererWeapon", () => {
+      expect(
+        mountedFacts.find("RadioGroup[name='murdererWeapon']").length
+      ).toBe(1);
+    });
+
+    it("renders a RadioGroup for 38Prints", () => {
+      expect(mountedFacts.find("RadioGroup[name='38Prints']").length).toBe(1);
+    });
+
+    it("renders a RadioGroup for 45Prints", () => {
+      expect(mountedFacts.find("RadioGroup[name='45Prints']").length).toBe(1);
+    });
+
+    it("calls updatePlayerClues with correct data for murdererSex changes", () => {
       const input = mountedFacts.find("input[name='murdererSex']").first();
-      const sampleSex = props.game.gameData.sheets[2].clueSelections.sex;
       input.simulate("change", { target: { checked: true, value: "male" } });
       expect(props.updatePlayerClues.mock.calls.length > 0);
       expect(props.updatePlayerClues.mock.calls[0][0]).toBe(2);
@@ -75,5 +106,57 @@ describe("Facts", () => {
         "male"
       );
     });
-  });
+
+    it("calls updatePlayerClues with correct data for murdererSide changes", () => {
+        const input = mountedFacts.find("input[name='murdererSide']").first();
+        input.simulate("change", { target: { checked: true, value: "west" } });
+        expect(props.updatePlayerClues.mock.calls.length > 0);
+        expect(props.updatePlayerClues.mock.calls[1][0]).toBe(2);
+        expect(props.updatePlayerClues.mock.calls[1][1].clueSelections.hiding.side).toBe(
+          "west"
+        );
+      });
+
+      it("calls updatePlayerClues with correct data for murdererTown changes", () => {
+        const input = mountedFacts.find("input[name='murdererTown']").first();
+        input.simulate("change", { target: { checked: true, value: "midtown" } });
+        expect(props.updatePlayerClues.mock.calls.length > 0);
+        expect(props.updatePlayerClues.mock.calls[2][0]).toBe(2);
+        expect(props.updatePlayerClues.mock.calls[2][1].clueSelections.hiding.town).toBe(
+          "midtown"
+        );
+      });
+
+      it("calls updatePlayerClues with correct data for murdererWeapon changes", () => {
+        const input = mountedFacts.find("input[name='murdererWeapon']").first();
+        input.simulate("change", { target: { checked: true, value: ".45" } });
+        expect(props.updatePlayerClues.mock.calls.length > 0);
+        expect(props.updatePlayerClues.mock.calls[3][0]).toBe(2);
+        expect(props.updatePlayerClues.mock.calls[3][1].clueSelections.weapons.used).toBe(
+          ".45"
+        );
+      });
+
+      it("calls updatePlayerClues with correct data for 38Prints changes", () => {
+        const input = mountedFacts.find("input[name='38Prints']").first();
+        const sampleSex = props.game.gameData.sheets[2].clueSelections.sex;
+        input.simulate("change", { target: { checked: true, value: "even" } });
+        expect(props.updatePlayerClues.mock.calls.length > 0);
+        expect(props.updatePlayerClues.mock.calls[4][0]).toBe(2);
+        expect(props.updatePlayerClues.mock.calls[4][1].clueSelections.weapons[".38"].fingerprint).toBe(
+          "even"
+        );
+      });
+
+      it("calls updatePlayerClues with correct data for 45Prints changes", () => {
+        const input = mountedFacts.find("input[name='45Prints']").first();
+        const sampleSex = props.game.gameData.sheets[2].clueSelections.sex;
+        input.simulate("change", { target: { checked: true, value: "odd" } });
+        expect(props.updatePlayerClues.mock.calls.length > 0);
+        expect(props.updatePlayerClues.mock.calls[5][0]).toBe(2);
+        expect(props.updatePlayerClues.mock.calls[5][1].clueSelections.weapons[".45"].fingerprint).toBe(
+          "odd"
+        );
+      });
+            });
 });
